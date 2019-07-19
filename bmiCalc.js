e6b90e9d247668d8e056a4c0eeddf2bbb4920e29
brianhaucke <brian.haucke@gmail.com>
2019-07-19 16:44:21 -0700

    add more simple solution to bmiCalc

--- bmiCalc.js
+++ bmiCalc.js
@@ -2,3 +2,9 @@ function bmiCalculator(weight, height) {
      var bmi = Math.round(weight / (Math.pow(height, 2)));
      return bmi;
 }
+
+// Simple solution:
+// function bmiCalculator(weight, height){
+//     var bmi = weight / (height * height)
+//     return bmi;
+// }
